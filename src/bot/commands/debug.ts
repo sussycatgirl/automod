ce5f27cda08c52bf06880700281e936150adcc21
JandereDev <me@janderedev.xyz>
2021-12-05 15:21:56 +0100

    fix typo

--- src/bot/commands/debug.ts
+++ src/bot/commands/debug.ts
@@ -8,7 +8,7 @@ export default {
     run: (message: MessageCommandContext, args: string[]) => {
         message.reply(`Server ID: ${message.channel?.server_id || 'None'}\n`
                     + `Server context: ${message.serverContext._id} `
-                        + `(${message.serverContext._id == message.channel?.server_id ? ' This server' : message.serverContext.name})\n`
+                        + `(${message.serverContext._id == message.channel?.server_id ? 'This server' : message.serverContext.name})\n`
                     + `Channel ID: ${message.channel_id}\n`
                     + `User ID: ${message.author_id}`);
     }
