4b13005bccd0a3a6613b71a0fe1c2e2a7c683b3a
janderedev <me@janderedev.xyz>
2022-03-15 18:13:05 +0100

    make home page legible

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -1,10 +1,23 @@
 import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
 
 const Home: FunctionComponent = () => {
     return (
-        <div>
-            <h1>todo: web design</h1>
-            <a href='/dashboard'>sign in</a>
+        <div style={{ marginLeft: '12px' }}>
+            <div style={{ display: 'flex', padding: '16px 0' }}>
+                <img src='/src/favicon.svg' style={{ height: '40px' }} />
+                <h1 style={{ color: 'var(--foreground)', margin: '0', paddingLeft: '8px' }}>Automod Web UI</h1>
+            </div>
+
+            <span style={{ color: 'var(--foreground)' }}>
+                This is a <b>work-in-progress</b> Web UI for the Automod Revolt bot.
+                <br />
+                <Link to='/dashboard'>
+                    Open Dashboard
+                </Link> or <a href="https://app.revolt.chat/bot/01FHGJ3NPP7XANQQH8C2BE44ZY" target='_blank'>
+                    add the bot to your server.
+                </a>
+            </span>
         </div>
     );
 }
