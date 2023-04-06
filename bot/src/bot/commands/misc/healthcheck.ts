0fd98744db8df687427ce89d300f886086febde9
Lea <me@janderedev.xyz>
2023-04-06 12:24:39 +0200

    add healthcheck to bot

---
+++ bot/src/bot/commands/misc/healthcheck.ts
@@ -0,0 +1,14 @@
+import CommandCategory from "../../../struct/commands/CommandCategory";
+import SimpleCommand from "../../../struct/commands/SimpleCommand";
+import MessageCommandContext from "../../../struct/MessageCommandContext";
+
+export default {
+    name: 'healthcheck',
+    aliases: null,
+    description: 'Health check',
+    category: CommandCategory.Misc,
+    run: async (message: MessageCommandContext, args: string[]) => {
+        const msg = await message.reply('Health check success: ' + args.join(' '));
+        setTimeout(() => msg?.delete().catch(e => console.error), 5000);
+    }
+} as SimpleCommand;
